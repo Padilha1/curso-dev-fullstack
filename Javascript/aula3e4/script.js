b697a786522865094e72ceb9e92305cb6a4e1fa7
Matheus Padilha <82119551+Padilha1@users.noreply.github.com>
2022-01-16 15:34:59 -0300

    JS Exercises

---
+++ Javascript/aula3e4/script.js
@@ -0,0 +1,24 @@
+
+// comentario 1 linha
+/*
+quantas linhas quiser
+
+  Alguns Comandos
+break
+continue
+debugger
+do ... while
+for 
+function
+if ...
+return
+switch
+try ...
+var
+let
+const
+
+*/
+
+console.log('hello world');
+console.log('hello world2');
